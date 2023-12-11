9c2139be0f11d8913e52840fe5d8d9145bb86b93
rakshit_nanda <rakshitnanda84@gmail.com>
2023-12-12 01:48:15 +0530

    create and run server

--- mernstack/src/components/Header.jsx
+++ mernstack/src/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
                 <Link to='/about'>
                 <li>About</li>
                 </Link>
-                <Link to='/Sign In'>
+                <Link to='/Sign-in'>
                 <li>Sign In</li>
                 </Link>
             </ul>
