721fd959e7979cd08b38752dc89c6f270bbf3d1f
rakshit_nanda <rakshitnanda84@gmail.com>
2023-12-11 22:58:27 +0530

    add routes and create pages

--- mernstack/src/App.jsx
+++ mernstack/src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUP from "./pages/SignUP"
+import Profile from "./pages/Profile"
 
 export default function APP() {
-  return (
-    <div>APP</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/about" element={<About />}/>
+    <Route path="/sign-in" element={<SignIn />}/>
+    <Route path="/sign-up" element={<SignUP />}/>
+    <Route path="/profile" element={<Profile />}/>
+  </Routes>
+  </BrowserRouter>
 }
 
